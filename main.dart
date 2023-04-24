04e56dc37ac2d1009e9c91c04c3e09bdbf704662
Woosung Kim <devlunch4@gmail.com>
2023-04-23 22:29:39 -0400

    #2.0 Basic Data Types (02:12)

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
 void main() {
-    // 'Consts' in JavaScript or TypeScript
-    // are similar to 'final' in Dart.
-
-    const name = 'lunch';
-    print(name);
-    name = 'change'; // Error
+    String name = 'lunch';
+    print(name.length); //
+    bool alive = true;
+    print(alive.runtimeType); //
+    int age = 100;
+    print(age.isNaN); //
+    double money = 99.9;
+    print(money.round()); //
+    num x = 12; //In Dart num ist int and double
+    x = 1.1;
+    print(x.hashCode); //
 }
