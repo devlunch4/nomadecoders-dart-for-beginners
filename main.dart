9702b5b3851d20029c3b03841a1b347f275c43fe
Woosung Kim <devlunch4@gmail.com>
2023-04-24 23:51:00 -0400

    #3.1 Named Parameters (08:54)

--- main.dart
+++ main.dart
@@ -1,21 +1,22 @@
-String sayHello(String name) {
-    return "Hello, $name, Nice to meet you.";
+String sayHello({String name = "NONE", int age = 0, String country = "NONE"}) {
+    return "Hello, $name, You are $age, and  you come from $country";
 }
 
-// fat arrow sysntax ex1
-String sayHelloFAS(String name) => "Hello, $name, Nice to meet you.";
-
-// fat arrow sysntax ex2
-num add(num a, num b) => a + b;
+String sayHelloRequired(
+        {required String name, required int age, required String country}) {
+    return "Hello, $name, You are $age, and  you come from $country";
+}
 
 void main() {
-    print(sayHello("lunch"));
-    print(sayHelloFAS("lunch!!!"));
-    print(add(1, 2));
+    print(sayHello(name: "lunch", age: 100, country: "Canada"));
+    //NULL SAFE
+    print(sayHello());
+    //required
+    print(sayHelloRequired(name: "required lunch", age: 100, country: "Canada"));
 }
 
 // PRINT
 // >>>
-// Hello, lunch, Nice to meet you.
-// Hello, lunch!!!, Nice to meet you.
-// 3
+// Hello, lunch, You are 100, and  you come from Canada
+// Hello, NONE, You are 0, and  you come from NONE
+// Hello, required lunch, You are 100, and  you come from Canada
