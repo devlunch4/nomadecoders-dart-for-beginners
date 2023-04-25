736d0c270f83a791f9fa9b089408b4342000c06e
Woosung Kim <devlunch4@gmail.com>
2023-04-25 15:12:14 -0400

    #3.3 Optional Positional Parameters (03:02)
    #3.2 is recap

--- main.dart
+++ main.dart
@@ -1,22 +1,11 @@
-String sayHello({String name = "NONE", int age = 0, String country = "NONE"}) {
-    return "Hello, $name, You are $age, and  you come from $country";
-}
-
-String sayHelloRequired(
-        {required String name, required int age, required String country}) {
-    return "Hello, $name, You are $age, and  you come from $country";
-}
+String sayHello(String name, int age, [String? country = 'Canada']) =>
+    "Hello, $name, You are $age, and  you come from $country";
 
 void main() {
-    print(sayHello(name: "lunch", age: 100, country: "Canada"));
-    //NULL SAFE
-    print(sayHello());
-    //required
-    print(sayHelloRequired(name: "required lunch", age: 100, country: "Canada"));
+    var results = (sayHello("lunch", 100));
+    print(results);
 }
 
 // PRINT
 // >>>
 // Hello, lunch, You are 100, and  you come from Canada
-// Hello, NONE, You are 0, and  you come from NONE
-// Hello, required lunch, You are 100, and  you come from Canada
