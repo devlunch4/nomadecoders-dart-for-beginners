a5322e880d43044cca719f61be1a4923bc1ff4fc
Woosung Kim <devlunch4@gmail.com>
2023-04-25 19:19:34 -0400

    #4.1 Constructors (03:58)

--- main.dart
+++ main.dart
@@ -1,19 +1,24 @@
 class Player {
-    final String name = 'lunch';
+    final String name;
     int xp = 9999;
 
+    Player(this.name, this.xp);
+
     void sayHello() {
-        print("Hi my name is $name");
-  }
+    print("Hi my name is $name");
+    }
 }
 
 void main() {
-    var player = Player();
+    var player = Player('lunch', 9999);
     print(player.name);
     player.sayHello();
+    var player2 = Player('lunch2', 2222);
+    player2.sayHello();
 }
 
 // PRINT
 // >>>
 // lunch
 // Hi my name is lunch
+// Hi my name is lunch2
