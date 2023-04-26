ce8030c64a3bf34978b0f2803b7b85c4d3ee239e
Woosung Kim <devlunch4@gmail.com>
2023-04-25 20:07:20 -0400

    #4.5 Cascade Notation (03:13)
    $4.4 is recap

--- main.dart
+++ main.dart
@@ -1,30 +1,13 @@
 class Player {
-  final String name;
-  int xp, age;
+  String name;
+  int xp;
   String team;
 
   Player({
     required this.name,
     required this.xp,
     required this.team,
-    required this.age,
   });
-
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
-
-  Player.createRedPlayer(
-    String name,
-    int age,
-  )   : this.age = age,
-        this.name = name,
-        this.team = 'red',
-        this.xp = 0;
 
   void sayHello() {
     print("Hi my name is $name");
@@ -32,24 +15,19 @@ class Player {
 }
 
 void main() {
-  var bluePlayer = Player.createBluePlayer(
+  var lunch = Player(
     name: 'lunch',
-    age: 100,
-  );
-  print(bluePlayer.name);
-  bluePlayer.sayHello();
-
-  var redPlayer = new Player.createRedPlayer(
-    'lunchRed',
-    100,
-  );
-  print(redPlayer.name);
-  redPlayer.sayHello();
+    xp: 120,
+    team: 'red',
+  )
+    ..name = 'john'
+    ..xp = 9999
+    ..team = 'blue'
+    ..sayHello();
+  print(lunch.name);
 }
 
 // PRINT
 // >>>
-// lunch
-// Hi my name is lunch
-// lunchRed
-// Hi my name is lunchRed
+// Hi my name is john
+// john
